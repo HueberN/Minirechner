ab84d7a16ea1c794a001a19404355a444db3e337
Hueber Noemi <hueber.n.02@gmail.com>
2020-04-24 08:18:47 +0200

    Initial commit

--- Minirechner/ConsoleApp1/Program.cs
+++ Minirechner/ConsoleApp1/Program.cs
@@ -10,6 +10,28 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            
         }
+        static int Addiere(int Zahl1, int Zahl2)
+        {
+            return Zahl1 + Zahl2;         
+        }
+
+        static int Subtrahiere(int Zahl1, int Zahl2)
+        {
+            return Zahl1 - Zahl2;
+        }
+
+        static int Multipliziere(int Zahl1, int Zahl2)
+        {
+            return Zahl1 * Zahl2;
+        }
+
+        static float Dividiere(int Zahl1, int Zahl2)
+        {
+            return Zahl1 / (float)Zahl2;
+        }
+
+
     }
 }
